d0547368d606c349f53f655ea9676387d8d57bb2
ehsan <bemaxima@gmail.com>
2021-07-18 20:35:52 +0200

    Apply persian charcaters.

--- src/components/pages/basket/InputNumber.tsx
+++ src/components/pages/basket/InputNumber.tsx
@@ -1,18 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TextField from '@material-ui/core/TextField'
+import { useEffect } from 'react';
 
-export default React.forwardRef<any, any>((props, ref) => {
+const Persian = {
+  0: '۰',
+  1: '۱',
+  2: '۲',
+  3: '۳',
+  4: '۴',
+  5: '۵',
+  6: '۶',
+  7: '۷',
+  8: '۸',
+  9: '۹'
+};
+
+const English = {
+  '۰': '0',
+  '۱': '1',
+  '۲': '2',
+  '۳': '3',
+  '۴': '4',
+  '۵': '5',
+  '۶': '6',
+  '۷': '7',
+  '۸': '8',
+  '۹': '9'
+}
+
+
+export default (props) => {
+  const [val, setVal] = useState(props.value || '');
   const handleChange = (e) => {
-    console.log('====================================');
-    console.log(e.target.value);
-    console.log('====================================');
-    if (props.onChange) {
-      props.onChange(e);
+    const newValue = (e.target.value || '').toString();
+    const oldValue = (props.value || '').toString();
+    if (newValue.length > oldValue.length) {
+      const lastCharacter = newValue[newValue.length - 1];
+      if(!Persian[lastCharacter]) {
+        return;
+      }
+      setVal(`${newValue}${(Persian[lastCharacter] || lastCharacter).slice(0, newValue.length - 2)  || lastCharacter}`)
+   
     }
+    props.onChange({
+      ...e,
+      target: {
+        ...e.target,
+        value: newValue.split('').map(x => English[x] || x).join('')
+      }
+    })
   }
 
+  useEffect(
+    () => {
+      setVal(() => {
+        return (props.value || '').toString().split('').map(c => Persian[c] || c).join('')
+      })
+    },
+    [props.value]
+  );
+
   return (
-    <TextField {...props} onChange={handleChange} ref={ref} />
+    <TextField {...props}  value={val} onChange={handleChange} />
   )
 
-})
+}
