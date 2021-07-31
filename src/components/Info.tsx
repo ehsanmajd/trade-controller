1f2bede1a28a39dcd89c6761e1f1cb0285578102
ehsan <bemaxima@gmail.com>
2021-07-31 12:39:20 +0200

    Remove unused imports.

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Button, IconButton } from '@material-ui/core';
 import Snackbar from '@material-ui/core/Snackbar/Snackbar';
-import CloseIcon from '@material-ui/icons/Close';
 
 interface Props {
   open: boolean;
