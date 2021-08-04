40030ba1b28db3b0466907b70836556a77971eef
ehsan <bemaxima@gmail.com>
2021-08-04 23:31:13 +0200

    Refresh the data every 20 seconds.

--- src/hooks/useBaskets.ts
+++ src/hooks/useBaskets.ts
@@ -1,18 +1,21 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { BasketModel } from "../types/baskets";
 import * as service from '../service'
+import { useInterval } from "./useInterval";
 
 export function useBaskets() {
   const [refreshTime, setRefreshTime] = useState<Date | null>(null);
   const [baskets, setBaskets] = useState<BasketModel[]>([]);
 
-  async function refresh() {
+  const refresh = useCallback(async function () {
     const timeStamp = new Date();
     const baskets: BasketModel[] = await service.getBaskets();
     setRefreshTime(timeStamp);
     setBaskets(baskets);
     return baskets;
-  }
+  }, []);
+
+  useInterval(refresh, 20 * 1000);
 
   return {
     refreshTime,
---
+++ src/hooks/useInterval.ts
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+export function useInterval(fn: Function, interval: number) {
+  useEffect(
+    () => {
+      const timerId = setInterval(
+        () => {
+          fn();
+        },
+        interval
+      );
+      return () => clearInterval(timerId);
+    },
+    [interval, fn]
+  );
+}
