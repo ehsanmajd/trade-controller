8daed7677d8975ec168f1f06095b098b230846bb
ehsan <bemaxima@gmail.com>
2021-08-10 21:58:01 +0200

    Fix error handler.

--- src/hooks/useBaskets.ts
+++ src/hooks/useBaskets.ts
@@ -1,21 +1,29 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { BasketModel } from "../types/baskets";
 import * as service from '../service'
 import { useInterval } from "./useInterval";
 import { useUserContext } from "../context/UserContext";
+import { usePrevious } from "./usePrevious";
 
 export function useBaskets() {
+  const rendered = useRef(false);
   const [refreshTime, setRefreshTime] = useState<Date | null>(null);
   const [baskets, setBaskets] = useState<BasketModel[]>([]);
   const { data: user } = useUserContext();
 
+  useEffect(() => {
+    rendered.current = true;
+  }, []);
+
   const refresh = useCallback(async function () {
     const timeStamp = new Date();
-    const baskets: BasketModel[] = await service.getBaskets();
+    let baskets: BasketModel[] = await service.getBaskets();
+    if (!rendered.current) {
+      baskets = baskets.filter(x => x.success);
+    }
     setRefreshTime(timeStamp);
     setBaskets(baskets);
     return baskets;
-
   }, [user]);
 
   useInterval(refresh, 20 * 1000);
