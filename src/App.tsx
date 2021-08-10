b558f1205d292997851199857cd3403d8a0836e9
ehsan <bemaxima@gmail.com>
2021-08-10 21:06:58 +0200

    Code cleanup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
 import './App.css';
-import Main from './pages/Main';
-import Admin from './pages/Admin';
-import SignIn from './pages/SignIn';
-import Setting from './pages/Setting';
 import {
   BrowserRouter as Router,
   Switch,
@@ -10,11 +6,7 @@ import {
 } from "react-router-dom";
 import { UserContext, UserContextDataModel } from './context/UserContext';
 import React from 'react';
-import Auth from './components/Auth';
-import SignOut from './pages/SignOut';
-import Summary from './pages/Summary';
 import DefaultPage from './pages/DefaultPage';
-import BasketProvider from './components/BasketProvider';
 import LoggedInUserRouter from './components/Routers/LoggedInUserRouter';
 import GuestUserRouter from './components/Routers/GuestUserRouter';
 
