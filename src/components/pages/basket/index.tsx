44c5c34c00a53a0216c737fb079f9094163c2680
ehsan <bemaxima@gmail.com>
2021-08-21 12:13:59 +0200

    Avoid breaking.

--- src/components/pages/basket/index.tsx
+++ src/components/pages/basket/index.tsx
@@ -108,7 +108,7 @@ export default function Basket() {
   function getExpertName(model: ParameterType[]): string {
     const symbol = model.find(x => x.name === 'symbol')?.type;
 
-    const strategy = model.find(x => x.name === 'strategy_serial')?.value.toString() || 'Unknow';
+    const strategy = model.find(x => x.name === 'strategy_serial')?.value?.toString() || 'Unknow';
     return `${strategy} ${symbol ? `(${symbol})` : ''}`
   }
 
