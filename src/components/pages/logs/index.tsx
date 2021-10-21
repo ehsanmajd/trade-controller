6e9b77447f809bf91c76490720e2940ddd6cc6f9
ehsan <bemaxima@gmail.com>
2021-10-21 19:35:58 +0200

    Add chevrons.

--- src/components/pages/logs/index.tsx
+++ src/components/pages/logs/index.tsx
@@ -1,10 +1,13 @@
-import { Grid, Paper, Table, TableBody, TableCell, TableHead, TableRow, TextField } from '@material-ui/core';
+import { createStyles, Grid, makeStyles, Paper, Table, TableBody, TableCell, TableHead, TableRow, TextField, Theme } from '@material-ui/core';
 import { Autocomplete } from '@material-ui/lab';
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { useBasketsContext } from '../../../context/BasketsContext';
 import { getExpertName } from '../../../utils/expert';
 import * as services from '../../../service';
+import IconButton from '@material-ui/core/IconButton';
+import Back from '@material-ui/icons/ArrowBack';
+import Forward from '@material-ui/icons/ArrowForward';
 
 interface LogModel {
   id: string;
@@ -27,17 +30,47 @@ const visualize = (records: { name: string, value: unknown }[]) => {
     }, '');
 }
 
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    selector: {
+      flexGrow: 1,
+      paddingTop: '48px',
+      borderBottomStyle: 'solid',
+      borderBottomWidth: '1px',
+      borderColor: theme.palette.grey[500],
+      paddingBottom: '48px'
+    },
+    chevron: {
+      fontSize: '14px'
+    }
+  }),
+);
+
 const Index: React.FC = () => {
-  const { data, refresh } = useBasketsContext();
+  const classes = useStyles();
+  const { data, hasError, refresh } = useBasketsContext();
   const [selectedBasket, setSelectedBasket] = useState<string | null>(null);
   const [logs, setLogs] = useState<LogModel[]>([]);
 
   const baskets = data?.baskets || [];
   const basket = baskets.find(x => x.basketId === selectedBasket);
+  const index = baskets.findIndex(x => x.basketId === selectedBasket);
 
   const handleBasketChange = (_, value) => {
     setSelectedBasket(value?.basketId);
   }
+
+  function navigate(mode: 'back' | 'forward') {
+    setSelectedBasket(mode === 'back' ?
+      baskets[index - 1].basketId :
+      baskets[index + 1].basketId
+    );
+  }
+
+  const backDisabled = index <= 0;
+  const nextDisabled = (index === -1 && !basket) || index === baskets.length - 1;
+
 
   useEffect(
     () => {
@@ -65,7 +98,7 @@ const Index: React.FC = () => {
 
   useEffect(
     () => {
-      if (!data) {
+      if (data.baskets.length === 0) {
         refresh();
       }
     },
@@ -74,16 +107,38 @@ const Index: React.FC = () => {
 
   return (
     <Paper>
-      <Grid container>
-        <Autocomplete
-          value={basket || { name: '' }}
-          id="combo-box-demo"
-          options={baskets}
-          getOptionLabel={basket => basket.name}
-          style={{ flex: '1', maxWidth: '300px' }}
-          onChange={handleBasketChange}
-          renderInput={(params) => <TextField {...params} label="Select your basket" variant="outlined" />}
-        />
+      <Grid container justify='center' className={classes.selector}>
+        <Grid md={6} xs={6} container justify='center'>
+          <Autocomplete
+            disabled={hasError}
+            value={basket || { name: '' }}
+            id="combo-box-demo"
+            options={baskets}
+            getOptionLabel={basket => basket.name}
+            style={{ flex: '1', maxWidth: '300px' }}
+            onChange={handleBasketChange}
+            renderInput={(params) => <TextField {...params} label="Select your basket" variant="outlined" />}
+          />
+        </Grid>
+        <Grid md={6} xs={6} container justify='space-evenly'>
+          <Grid md={6} xs={6}>
+            <IconButton
+              disabled={backDisabled}
+              onClick={() => navigate('back')}
+            >
+              <span className={classes.chevron}>Back</span>&nbsp;
+              <Back color={backDisabled ? 'disabled' : 'primary'} />
+            </IconButton>
+          </Grid>
+          <Grid md={6} xs={6}>
+            <IconButton
+              onClick={() => navigate('forward')}
+              disabled={nextDisabled}>
+              <Forward color={nextDisabled ? 'disabled' : 'primary'} />&nbsp;
+              <span className={classes.chevron}>Next</span>
+            </IconButton>
+          </Grid>
+        </Grid>
       </Grid>
       {
         basket?.parameters.map(expert => {
