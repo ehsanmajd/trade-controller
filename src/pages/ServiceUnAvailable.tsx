6d6319065edd5a5dea6a396fcee328e78b1746e5
Ehsan Majdabadi Farahani <ehsan.farahani@bestseller.com>
2023-02-09 17:00:09 +0100

    Fix user redirection.

--- src/pages/ServiceUnAvailable.tsx
+++ src/pages/ServiceUnAvailable.tsx
@@ -1,5 +1,18 @@
+import { useEffect } from "react";
+import { useHistory } from 'react-router-dom';
+import { useUserContext } from "../context/UserContext";
 
 const ServiceUnAvailable = () => {
+  const user = useUserContext();
+  const history = useHistory();
+  useEffect(
+    () => {
+      if(user.data.loggedIn) {
+        history.push('/v1/dashboard/home');
+      }
+    },
+    [user.data.loggedIn, history]
+  )
   return (
     <h1>Service Unavailable</h1>
   );
